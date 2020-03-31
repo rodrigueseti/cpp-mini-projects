04dba25f6c45f50926624fe82e95729cf786dae4
Victor Rodrigues <rodrigueseti@outlook.com>
2020-03-31 15:40:35 -0300

    Cadastro de album funcionando
    Cadastro de album funcionando, alguns bugs corrigidos, bibliotecas adicionadas

--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
 #include<stdio.h> 
 #include<string.h> // stricmp();
-#include<conio.h> // getche();
+#include<conio2.h> // getche(), clrscr();
 #include<ctype.h> // toupper();
 
-//#define maxalbum 100
-
+#define MAX_ALBUM 3
 
 struct albumMusical{
 	
@@ -12,70 +11,77 @@ struct albumMusical{
 
 };
 
-
-bool buscaAlbum(struct albumMusical *am, int tl, char *nome){
+bool buscaAlbum(struct albumMusical *am, int &tl, char *nome){
 	
 	int i = 0;
 	while(i < tl && stricmp(nome,am[i].nomeAlbum) != 0)
 		i++;
 		
-	if(i < tl){
-		printf("true");
-		return true;
-	}
-	return false;
+	if(i < tl) // se i menor que tl significa que album já existe
+		return false;
+	return true;
 }
 
-int cadastraNovoAlbum(struct albumMusical *am, int tl){
+int cadastraNovoAlbum(struct albumMusical *am, int &tl){
 	
 	char nome[50];
 	
-	if(tl < 100){
+	// If verifica se ha capacidade no vetor de structs
+	if(tl < MAX_ALBUM){ 
 		
+		printf("\nNome: ");
 		gets(nome);
 		
 		if(buscaAlbum(am, tl, nome)){
-		printf("Cadastro");
+			printf("Cadastro realizado");
+			strcpy(am[tl].nomeAlbum, nome);
+			tl++;
+			getch();
 		}
-		else
-			printf("\nAlbum nao encontrado");
+		else{
+			printf("Album ja existe");
+			getch();
+		}
 	}
-	else
+	else{
 		printf("\nLimite atingido");
+		getch();
+	}
 }
-	
-	
 
 char menu(void){
 	
-	printf("\nEstrobos's Som Dashboard");
+	clrscr();
+	printf("ESTROBOS'S SOM DASHBOARD\n");
 	printf("\n[1] Cadastrar Novo Album");
-	printf("\n[ESC] Finalizar");
+	printf("\n[ESC] Finalizar\n");
 	
-	return toupper(getche());	
+	return toupper(getch());	
 }
 
 int main(){
 	
-	struct albumMusical am[100];
+	struct albumMusical am[MAX_ALBUM];
 	int albumQtde = 0;
 	char opc;
 	
 	do{
 		opc = menu();
-		
+		clrscr();
 		switch(opc){
 			
 			case '1' :
 				printf("Cadastrar Novo Album");
 				cadastraNovoAlbum(am, albumQtde);
 				break;
+			case 27 :
+				printf("Fim");
+				break;
 			default:
 				printf("Opcao Invalida");
 		}
 		
 	}while(opc != 27);
-	
 	
 	return 0;
 }
